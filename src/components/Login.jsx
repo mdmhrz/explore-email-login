b15a3b50cc0e3a3e38cfcf3a987b27803eca5027
mdmhrz <razufreelance@gmail.com>
2025-06-06 18:38:46 +0600

    Login done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from '../firebase.init';
 
 const Login = () => {
+    const [errorMsg, setErrorMsg] = useState('')
+
+    const handleLogIn = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+
+        setErrorMsg("")
+
+        createUserWithEmailAndPassword(auth, email, password).then(res => {
+            console.log(res);
+        }).catch(error => {
+            setErrorMsg(error.message)
+        })
+
+    }
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='max-w-sm mx-auto mt-10 border-gray-100 border p-4 rounded-2xl'>
+            <h1 className='text-3xl font-bold text-center'>Please Login</h1>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                    <form onSubmit={handleLogIn}>
+                        <label className="label">Email</label>
+                        <input type="email" name='email' className="input" placeholder="Email" />
+                        <label className="label">Password</label>
+                        <input type="password" name='password' className="input" placeholder="Password" />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-neutral mt-4">Login</button>
+                    </form>
+                    {
+                        errorMsg && <p>{errorMsg}</p>
+                    }
+                </div>
+            </div>
         </div>
     );
 };
